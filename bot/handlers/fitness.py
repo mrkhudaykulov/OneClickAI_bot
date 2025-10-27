07f3584e6f1b3fdd10bc5675c61491ea3420d2c4
mrkhudaykulov <mr.khudaykulov@gmail.com>
2025-10-27 20:09:49 +0500

    Update fitness.py

--- bot/handlers/fitness.py
+++ bot/handlers/fitness.py
@@ -6,7 +6,7 @@ from ..keyboards import fitness_menu, main_menu, pose_post_actions
 from ..monetization import ensure_user_and_gate
 from ..states import Fitness
 from ..utils import download_best_photo_bytes, to_data_url
-from ..services.pose import overlay_pose
+# from ..services.pose import overlay_pose
 from ..services.vision import analyze_calories
 from ..services.text_gpt import generate_workout_tips, generate_week_plan, diet_from_photo_or_text
 
